cf75477490c2200d65d234669befe446a8e16f58
Andrew Bell <andrew@recursivechaos.com>
2018-03-29 19:13:45 -0500

    Drawer clipped under appbar

--- src/components/header.js
+++ src/components/header.js
@@ -5,29 +5,107 @@
  * Licensed under MIT License 2018. See license.txt for details.
  */
 import React from 'react';
+import PropTypes from 'prop-types';
 import {withStyles} from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
+import Icon from 'material-ui/Icon';
+import DevIcon from 'material-ui-icons/DeveloperMode';
+import Drawer from 'material-ui/Drawer';
+import List, {ListItem, ListItemIcon, ListItemText} from 'material-ui/List';
+import HomeIcon from 'material-ui-icons/Home';
+import CreateIcon from 'material-ui-icons/Create';
+import MailIcon from 'material-ui-icons/Email';
+
+const drawerWidth = 200;
 
 const styles = theme => ({
-    paper: {
-        padding: theme.spacing.unit * 2,
-        textAlign: 'left',
-        background: theme.palette.primary.main,
-    }
+    root: {
+        flexGrow: 1,
+        zIndex: 1,
+        overflow: 'hidden',
+        position: 'relative',
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawerPaper: {
+        position: 'relative',
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing.unit * 3,
+        minWidth: 0, // So the Typography noWrap works
+    },
+    toolbar: theme.mixins.toolbar,
+    menuButton: {
+        marginRight: 30,
+    },
 });
 
-const Header = (props) => (
-    <div className={props.classes.root}>
-        <AppBar position="static" color="primary">
-            <Toolbar>
-                <Typography variant="title" color="inherit">
-                    recursivechaos
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    </div>
-);
+const Header = (props) => {
+    const {classes} = props;
+    return (
+        <div className={classes.root}>
+            <AppBar position="absolute" className={classes.appBar}>
+                <Toolbar>
+                    <Icon className={classes.menuButton} color="inherit" aria-label="Menu">
+                        <DevIcon/>
+                    </Icon>
+                    <Typography variant="title" color="inherit" className={classes.flex}>
+                        recursivechaos
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            {/*Separate Class?*/}
+            <Drawer
+                variant="permanent"
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.toolbar}/>
+                <List>
+                    <div>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <HomeIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Home"/>
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <CreateIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Blog"/>
+                        </ListItem>
+                    </div>
+                    <div>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <MailIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Connect"/>
+                        </ListItem>
+                    </div>
+                </List>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.toolbar}/>
+                {/*This is where child classes would go*/}
+                <Typography noWrap>{'Some content will go here'}</Typography>
+            </main>
+        </div>
+    );
+};
+
+Header.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(Header)
