94c0f6a4a818aa95e5137629211802d420c993e1
Andrew Bell <andrew@recursivechaos.com>
2018-05-28 22:13:46 -0500

    Example Blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "recursivechaos home",
   },
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-netlify"],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-netlify",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: "markdown-pages",
+      },
+    },
+  ],
 };
